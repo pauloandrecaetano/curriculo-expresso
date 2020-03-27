757ff6531926b9c0c611f6130c93d2e82ef226a5
paulo.caetano <paulo.andre.pac@gmail.com>
2020-03-27 03:05:33 -0300

    Conclusão tutorial

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 const express = require('express');
-const port = 3000;
+const path = require('path');
+const createError = require('http-errors');
 const app = express();
+const port = 3000;
 
-app.get('/', (req, res, next) => {
-    res.send({
+app.use(express.static(path.join(__dirname, 'public')));
+
+const CurriculoController = require('./controllers/curriculo-controller');
+
+// Setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res, next) {
+    res.render('index', {
         title: "Meu primeiro servidor Express",
-        version: "0.0.0",
-        Filha: "Yasmin Dias Caetano"
+        version: "0.0.0"
     });
 });
 
+app.get('/curriculo',(req, res, next) => {
+    const curriculoData = CurriculoController.getData();
+    res.render('curriculo', curriculoData);
+});
+
+// 404
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 app.listen(port, err => {
-    console.log(`Server is listening on ${port}. Tendeu Yaya?`);
+    console.log(`Server is listening on ${port}`);
 });
